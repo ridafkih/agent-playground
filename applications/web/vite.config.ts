949be7c4423642274a717f6943d2e1da69eef6ea
Rida F'kih <hello@rida.dev>
2026-01-28 03:02:47 -0500

    chore: set `allowedHosts` to true

--- applications/web/vite.config.ts
+++ applications/web/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    allowedHosts: true,
     proxy: {
       "/api": "http://localhost:4000",
       "/health": "http://localhost:4000",
